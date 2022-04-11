57bf3c5bb6c9dfb7e0b611fa259e8bf1437fddff
Viktor.Igumenov <betrezenko@gmail.com>
2022-04-11 19:40:48 +0300

    Switching tabs with animation

---
+++ dist/js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded',() => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+    
+    // hide content for all tabs
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    // show content for selected tab
+    function showTabContent(i = 0) { // default value for i
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
